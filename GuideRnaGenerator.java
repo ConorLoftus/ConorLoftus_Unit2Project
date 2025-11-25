f59b4575836e067aef788033c38f5e5ed3cd0fcc
ConorLoftus <conorl7@nycstudents.net>
2025-11-25 08:47:08 -0500

    Add files via upload

--- GuideRnaGenerator.java
+++ GuideRnaGenerator.java
@@ -22,7 +22,59 @@ public class GuideRnaGenerator {
         return null;
     }
 
-    public static String locatePam(String sequence){
-        if()
+
+    public static String identifyPam(String sequence){
+        if(sequence.charAt(0)=='3'){
+            for (int i=1;i<sequence.length();i++){
+                if(sequence.charAt(i)=='G'&&sequence.charAt(i+1)=='G'){
+                    String pamSequence =  sequence.substring(i,i+2);
+                    return pamSequence;
+                }
+            }
+        }
+        if(sequence.charAt(0)=='5'){
+            for (int i=1;i<sequence.length();i++){
+                if(sequence.charAt(i)=='G'&&sequence.charAt(i+1)=='G'){
+                    String pamSequence =  sequence.substring(i-1,i+1);
+                    return pamSequence;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static int locatePam(String sequence){
+        if(sequence.charAt(0)=='3'){
+            for (int i=1;i<sequence.length();i++){
+                if(sequence.charAt(i)=='G'&&sequence.charAt(i+1)=='G'){
+                    int pamIndex = i;
+                    return pamIndex;
+                }
+            }
+        }
+        if(sequence.charAt(0)=='5'){
+            for (int i=1;i<sequence.length();i++){
+                if(sequence.charAt(i)=='G'&&sequence.charAt(i+1)=='G'){
+                    int pamIndex = i-1;
+                    return pamIndex;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static String complementaryStrand(String sequence){
+        String complementaryStrand = "";
+        for (int i=sequence.length()-1; i>=0;i--){
+            char base = sequence.charAt(i);
+            if (String.valueOf(base)=="3"){
+                complementaryStrand+=3;
+            }
+            if (String.valueOf(base)=="5"){
+                complementaryStrand+="5";
+            }
+            complementaryStrand+=complementaryBase(String.valueOf(base));
+        }
+        return complementaryStrand;
     }
 }
